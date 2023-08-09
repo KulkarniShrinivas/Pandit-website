7a09bd181aa53a044d50d75df7612ba0cdeb416e
kunal-arya <kunalaryad@gmail.com>
2023-08-09 21:27:59 +0530

    style: style fix

--- src/components/range-of-services/index.jsx
+++ src/components/range-of-services/index.jsx
@@ -7,8 +7,6 @@ import right from "../../assets/pRight.jpeg";
 const RangeOfServices = () => {
   return (
     <Box className="range-of-services">
-      <img src={left} className="pattern-left" />
-      <img src={right} className="pattern-right" />
       <div className="services-heading">
         <h2>
           We provide a range of <span>services</span>
