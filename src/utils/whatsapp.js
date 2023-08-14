65b0304123b2ac8fc2ed5f2c562a14108299bde4
Kunal-Arya <85951446+kunal-arya@users.noreply.github.com>
2023-08-14 21:51:40 +0530

    Update whatsapp.js

--- src/utils/whatsapp.js
+++ src/utils/whatsapp.js
@@ -1,12 +1,8 @@
 export const whatsappData = () => {
-  var phoneNumber = "7899148582"; // Replace with the phone number
-  var message = "Hello, this is a test message."; // Optional message
+  const phoneNumber = "7899148582"; // Replace with the phone number
+  const message = "Hello, this is a test message."; // Optional message
 
-  var whatsappUrl =
-    "https://api.whatsapp.com/send?phone=" +
-    phoneNumber +
-    "&text=" +
-    encodeURIComponent(message);
+  const whatsappUrl = `https://wa.me/${phoneNumber}?text=${message}`
 
   window.location.href = whatsappUrl;
 };
