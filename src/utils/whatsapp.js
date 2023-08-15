0e75c98ec5fb61fae155ade0f4410d7bcad91431
kunal-arya <kunalaryad@gmail.com>
2023-08-15 11:34:11 +0530

    whatsapp data merge conflict resolved

--- src/utils/whatsapp.js
+++ src/utils/whatsapp.js
@@ -1,14 +1,7 @@
 export const whatsappData = () => {
-<<<<<<< HEAD
-  var phoneNumber = "7982103213"; // Replace with the phone number
+  var phoneNumber = "7899148582"; // Replace with the phone number
 
   var whatsappUrl = "https://api.whatsapp.com/send?phone=" + phoneNumber;
-=======
-  const phoneNumber = "7899148582"; // Replace with the phone number
-  const message = "Hello, this is a test message."; // Optional message
-
-  const whatsappUrl = `https://wa.me/${phoneNumber}?text=${message}`
->>>>>>> 65b0304123b2ac8fc2ed5f2c562a14108299bde4
 
   window.location.href = whatsappUrl;
 };
