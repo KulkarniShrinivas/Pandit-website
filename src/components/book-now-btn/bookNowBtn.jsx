252ab190328cb062907742044a3de37b05ebebc8
Shrinivas Kulkarni <shrinivaskulkarni989@gmail.com>
2023-08-15 23:14:13 +0530

    updated

--- src/components/book-now-btn/bookNowBtn.jsx
+++ src/components/book-now-btn/bookNowBtn.jsx
@@ -1,8 +1,14 @@
 import "./bookNowBtn.scss";
 
 const BookNowBtn = () => {
+  const redirectToWhatsApp = () => {
+    // Replace with the actual WhatsApp link
+    const whatsappLink = "https://api.whatsapp.com/send?phone=7899148582";
+    window.location.href = whatsappLink;
+  };
+
   return (
-    <button class="btn" type="button">
+    <button class="btn" type="button" onClick={redirectToWhatsApp}>
       <strong>Book Now</strong>
       <div id="container-stars">
         <div id="stars"></div>
