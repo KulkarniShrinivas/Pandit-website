332e4d88e3853f6e4b5ce69ea8b82de53982a58c
Kunal-Arya <85951446+kunal-arya@users.noreply.github.com>
2023-08-16 12:16:40 +0530

    Update bookNowBtn.jsx

--- src/components/book-now-btn/bookNowBtn.jsx
+++ src/components/book-now-btn/bookNowBtn.jsx
@@ -1,22 +1,18 @@
 import "./bookNowBtn.scss";
+import { whatsappData } from "../../utils/whatsapp";
 
 const BookNowBtn = () => {
-  const redirectToWhatsApp = () => {
-    // Replace with the actual WhatsApp link
-    const whatsappLink = "https://api.whatsapp.com/send?phone=7899148582";
-    window.location.href = whatsappLink;
-  };
 
   return (
-    <button class="btn" type="button" onClick={redirectToWhatsApp}>
-      <strong>Book Now</strong>
+    <button className="bookNowBtn" type="button" onClick={() => whatsappData()}>
+      <strong className="strong">Book Now</strong>
       <div id="container-stars">
         <div id="stars"></div>
       </div>
 
       <div id="glow">
-        <div className="circle"></div>
-        <div className="circle"></div>
+        <div className="circleForBookNow"></div>
+        <div className="circleForBookNow"></div>
       </div>
     </button>
   );
