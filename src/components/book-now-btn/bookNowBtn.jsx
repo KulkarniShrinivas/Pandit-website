4982011a43a5a538d064774374f7a80a2e711910
Shrinivas Kulkarni <shrinivaskulkarni989@gmail.com>
2023-08-16 14:17:38 +0530

    updated

--- src/components/book-now-btn/bookNowBtn.jsx
+++ src/components/book-now-btn/bookNowBtn.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import { whatsappData } from "../../utils/whatsapp";
 import "./bookNowBtn.scss";
+import { whatsappData } from "../../utils/whatsapp";
 
 const BookNowBtn = () => {
 
   return (
     <button className="bookNowBtn" type="button" onClick={() => whatsappData()}>
-      BOOK NOW
+      <strong className="strong">Book Now</strong>
+      <div id="container-stars">
+        <div id="stars"></div>
+      </div>
+
+      <div id="glow">
+        <div className="circleForBookNow"></div>
+        <div className="circleForBookNow"></div>
+      </div>
     </button>
   );
 };
