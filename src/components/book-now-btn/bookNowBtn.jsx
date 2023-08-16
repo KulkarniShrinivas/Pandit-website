ad2fe54df649f150cd165636f8b71cd1f2b2a854
Shrinivas Kulkarni <shrinivaskulkarni989@gmail.com>
2023-08-16 14:01:45 +0530

    updated

--- src/components/book-now-btn/bookNowBtn.jsx
+++ src/components/book-now-btn/bookNowBtn.jsx
@@ -1,51 +1,14 @@
-
 import React from 'react';
 import { whatsappData } from "../../utils/whatsapp";
-import './bookNowBtn.scss'; // Make sure to adjust the path to your SCSS file
+import "./bookNowBtn.scss";
 
 const BookNowBtn = () => {
+
   return (
-    <button className="button" onClick={() => whatsappData()}>
-      <div className="display">
-        <div id="msg">Click Me</div>
-      </div>
-      <span></span>
-      <span></span>
+    <button className="bookNowBtn" type="button" onClick={() => whatsappData()}>
+      BOOK NOW
     </button>
   );
 };
 
 export default BookNowBtn;
-
-
-
-
-
-
-
-
-
-
-
-
-// import "./bookNowBtn.scss";
-// import { whatsappData } from "../../utils/whatsapp";
-
-// const BookNowBtn = () => {
-
-//   return (
-//     <button className="bookNowBtn" type="button" onClick={() => whatsappData()}>
-//       <strong className="strong">Book Now</strong>
-//       <div id="container-stars">
-//         <div id="stars"></div>
-//       </div>
-
-//       <div id="glow">
-//         <div className="circleForBookNow"></div>
-//         <div className="circleForBookNow"></div>
-//       </div>
-//     </button>
-//   );
-// };
-
-// export default BookNowBtn;
