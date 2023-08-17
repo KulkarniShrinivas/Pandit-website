83b4de865881ccc0f462238c083378334a3aeb79
kunal-arya <kunalaryad@gmail.com>
2023-08-17 21:31:20 +0530

    all puja services page updated

--- src/components/all-services/service-card/styles.js
+++ src/components/all-services/service-card/styles.js
@@ -26,9 +26,8 @@ export const serviceCardStyles = (src) => ({
     width: "100%",
     height: "80%",
     borderRadius: "17px 17px 0 0",
-    background: `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
-    url(${src})
-      center/cover no-repeat`,
+    backgroundImage: `linear-gradient(to bottom, rgba(245, 246, 252, 0.52), rgba(117, 19, 93, 0.73)),url(${src})`,
+    backgroundBlendMode: "multiply",
     backgroundPosition: "top",
   },
 
