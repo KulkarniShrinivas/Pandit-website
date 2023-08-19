793cf5cf059fd93d9c4e13d5ac4922eac517eaaf
kunal-arya <kunalaryad@gmail.com>
2023-08-19 21:31:32 +0530

    about us updated

--- src/components/about-us-page/about-us-main/aboutUsMain.jsx
+++ src/components/about-us-page/about-us-main/aboutUsMain.jsx
@@ -1,16 +1,18 @@
 import { aboutUsPageData } from "../../../data/about-us-page-data";
 import "./aboutUsMain.scss";
-// import ParticlesBackground from "./particlesConfig/ParticlesBackground";
+import { ParticlesBackground } from "./particlesConfig/particlesConfig";
+import Tilt from "react-parallax-tilt";
 
 const AboutUsMain = () => {
   return (
     <div className="about-us-main">
-      {/* <ParticlesBackground /> */}
-
-      <div className="about-us-main-img">
-        <img src={aboutUsPageData.main.pandit_img} />
-        <h1>{aboutUsPageData.main.pandit_name}</h1>
-      </div>
+      <ParticlesBackground />
+      <Tilt>
+        <div className="about-us-main-img">
+          <img src={aboutUsPageData.main.pandit_img} />
+          <h1>{aboutUsPageData.main.pandit_name}</h1>
+        </div>
+      </Tilt>
     </div>
   );
 };
