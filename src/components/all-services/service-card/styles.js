d7c5cb5eab856b11bf03a60b7af7f229992d5bb1
kunal-arya <kunalaryad@gmail.com>
2023-08-22 07:31:30 +0530

    style: img changes

--- src/components/all-services/service-card/styles.js
+++ src/components/all-services/service-card/styles.js
@@ -31,6 +31,7 @@ export const serviceCardStyles = (src) => ({
       width: "100%",
       height: "100%",
       objectFit: "cover",
+      objectPosition: "top",
     },
 
     "& .gradient-overlay": {
